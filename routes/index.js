54fc9210888e57418f6ad2d8fa625ef73a92505e
Michał Bałdyga <q270311@gmail.com>
2023-03-11 22:03:38 +0100

    selectAllStuff to router added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const { json } = require('express');
+const express = require('express');
+const router = express.Router();
+const connection = require('../dboperation');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Select All Stuff */
+router.get("/stuff", (req, res, next) => {
+  connection.selectAllStuff().then((result) => {
+    res.json(result[0]);
+  });
 });
 
 module.exports = router;
