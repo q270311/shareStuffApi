c6be5e08e8758f92d0e36e0b8bd01d585510c190
Michał Bałdyga <q270311@gmail.com>
2023-03-11 22:12:38 +0100

    add API homepage

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../dboperation');
 
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Share Stuff API' });
+});
+
 /* Select All Stuff */
 router.get("/stuff", (req, res, next) => {
   connection.selectAllStuff().then((result) => {
