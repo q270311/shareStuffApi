fe80d009e7fe266f760fec7b35d20d507fee06a1
Michał Bałdyga <q270311@gmail.com>
2023-03-11 22:02:33 +0100

    dbconfig added

---
+++ dbconfig.js
@@ -0,0 +1,30 @@
+/* const config = {
+    user: "admin",
+    password: "Zaq12wsx",
+    server: "localhost",
+    database: "NORTHWND",
+    options: {
+      trustedconnection: true,
+      enableArithAbort: true,
+      trustServerCertificate: true,
+      instancename: "DESKTOP-ABS4IEB",
+    },
+    port: 1433,
+  }; */
+
+  const config = {
+    user: "q270311_SQLLogin_1",
+    password: "abocn6fr5l",
+    server: "sharestuff.mssql.somee.com",
+    database: "sharestuff",
+    options: {
+      trustedconnection: true,
+      enableArithAbort: true,
+      trustServerCertificate: true,
+      instancename: "",
+    },
+    port: 1433,
+  };
+   
+  module.exports = config;
+
