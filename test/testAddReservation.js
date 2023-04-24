7d1167e984959d55e678f6edc8b07c4e8375020b
Michał Bałdyga <q270311@gmail.com>
2023-04-24 21:44:21 +0200

    test adding reservation

--- test/testAddReservation.js
+++ test/testAddReservation.js
@@ -3,10 +3,11 @@
 (async () => {
     try {
         await axios.post("https://sharestuff.onrender.com/api/reservation",{
+            //const resutl = await axios.post("http://localhost:3000/api/reservation",{
             stuffId: 3,
-            startDate: "20230501",
-            endDate: "20230503"
-        });
+            startDate: "2023-05-01",
+            endDate: "2023-05-03",
+        });       
     } catch (error) {
         console.log(`Brak komunikacji z serwerem ${error}`)
     }
