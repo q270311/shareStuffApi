4500ddfb9f5d0bea8bf7642408e83ebae0964970
Michał Bałdyga <q270311@gmail.com>
2023-04-24 21:44:42 +0200

    source code formatted

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const { json } = require('express');
-const express = require('express');
+const { json } = require("express");
+const express = require("express");
 const router = express.Router();
-const connection = require('../dboperation');
+const connection = require("../dboperation");
 
 /* GET home page. */
-router.get('/api', (req, res, next) => {
-  res.render('index', { title: 'Share Stuff API' });
+router.get("/api", (req, res, next) => {
+  res.render("index", { title: "Share Stuff API" });
 });
 
 /* Select All Stuff */
@@ -30,19 +30,19 @@ router.get("/api/reservations/:id", (req, res, next) => {
 });
 
 /* insert Stuff */
-router.post("/api/stuff", (req, res, next) => {  
-  const sutff = { ...req.body }
+router.post("/api/stuff", (req, res, next) => {
+  const sutff = { ...req.body };
   connection.addStuff(sutff).then((result) => {
     res.status(201).json(result);
-  });  
+  });
 });
 
 /* insert reservation */
 router.post("/api/reservation", (req, res, next) => {
-  const reservation = { ...req.body }
+  const reservation = { ...req.body };
   connection.addReservation(reservation).then((result) => {
     res.status(201).json(result);
-  });  
+  });
 });
 
 module.exports = router;
